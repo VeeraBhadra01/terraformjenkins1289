af9ee70a7f7ea7a2bb685661a5a482c672acd5c8
Bhadra <bhadra9821@gmail.com>
2025-06-08 15:47:02 +0530

    new changes made

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "region" {
-    default = "us-east-1"
+  default = "us-east-1"
 }
 variable "vpc_cidr" {
-    default = "10.0.0.0/16"
+  default = "10.10.0.0/16"
 }
 variable "public_subnet_cidr" {
-    default = "10.0.1.0/24"
+  default = "10.10.2.0/24"
 }
